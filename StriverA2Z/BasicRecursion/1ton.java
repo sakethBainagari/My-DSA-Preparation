5d3c36f451b9651c5400980935af750a58295440
sakethBainagari <saketh7727@gmail.com>
2025-07-26 21:27:19 +0530

    1ton

---
+++ StriverA2Z/BasicRecursion/1ton.java
@@ -0,0 +1,13 @@
+class Main {
+    static void func(int i,int n){
+        if(i>n){
+            return;
+        }
+        System.out.println(i);
+        func(i+1,n);
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        func(1,n);
+    }
+}
