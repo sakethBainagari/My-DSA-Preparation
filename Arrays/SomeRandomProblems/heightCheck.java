db4045f8fa330d59b02beb3581d607f0d2d4bfa3
sakethBainagari <saketh7727@gmail.com>
2025-07-30 20:58:11 +0530

    height

---
+++ Arrays/SomeRandomProblems/heightCheck.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int n = heights.length;
+        int[] temp = new int[n];
+        for(int i=0;i<n;i++){
+            temp[i] = heights[i];
+        }
+        Arrays.sort(temp);
+        int count =0;
+        for(int i=0;i<n;i++){
+            if(heights[i] != temp[i]){
+                count++;
+            }
+        }
+
+        return count;
+        
+    }
+}
