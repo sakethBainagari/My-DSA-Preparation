0be1ead9077ea809b2d074ac447dd15bf7e1a178
sakethBainagari <saketh7727@gmail.com>
2025-07-31 21:28:15 +0530

    CountPairsoptimal

--- StriverA2Z/Arrays/SomeRandomProblems/CountPairsoptimal.java
+++ StriverA2Z/Arrays/SomeRandomProblems/CountPairsoptimal.java
@@ -1,3 +1,19 @@
-public class CountPairsoptimal {
-    
+class Solution {
+    public int countPairs(List<Integer> nums, int target) {
+        int n = nums.size();
+        int count = 0;
+        Collections.sort(nums);
+        int i = 0;
+        int j = n-1;
+        while(i< j){
+            if( nums.get(i) + nums.get(j) < target){
+                count += (j-i);
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return count;
+        
+    }
 }
