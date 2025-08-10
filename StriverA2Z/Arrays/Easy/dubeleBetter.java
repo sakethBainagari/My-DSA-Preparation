e1349ef5be4689ac1d827ea1f6aeb7939da1ef75
sakethBainagari <saketh7727@gmail.com>
2025-08-10 21:33:31 +0530

    dubeleBetter

---
+++ StriverA2Z/Arrays/Easy/dubeleBetter.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=0;
+        int n = nums.length;
+        //O(n)
+        for(int j=1;j<n;j++){
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+}
